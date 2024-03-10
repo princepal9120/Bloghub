30b05e71b541f9ae846be880d27aabaded39fc5d
Prince Pal <princepal9120@gmail.com>
2024-03-11 01:39:22 +0530

    error solve

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-module.exports = {
+
+
+export default defineConfig({
+  plugins: [react()],
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
-export default defineConfig({
-  plugins: [react()],
   
 });
