6ee389f5ca699cdce79670256e65443509506bbf
Prince Pal <princepal9120@gmail.com>
2024-03-11 01:29:54 +0530

    add some vite file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 export default defineConfig({
   plugins: [react()],
+  
 });
